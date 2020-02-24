65c35f85325fb21a9ae6227b5d2d1ee9dc403988
BenLirio <benjlirio@gmail.com>
2020-02-24 10:52:15 -0500

    Layout wirefram

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,48 +1,37 @@
-import {
-  Grid,
-  makeStyles,
-  Typography,
-  Fade,
-  Grow,
-  Box
-} from '@material-ui/core'
+import { makeStyles, Typography, Grid, Box } from '@material-ui/core'
 import React from 'react'
-import Service from '../../components/Services/Service'
-import Bread from '../../components/shared/Bread'
-import services from '../../js/services/services'
 
-const useStyles = makeStyles(theme => ({
-  media: {
-    height: 140
-  }
-}))
+const useStyles = makeStyles(theme => ({}))
 
 const index = () => {
   const classes = useStyles()
   return (
     <>
-      <Grid item xs={12}>
-        <Box marginTop={5} />
-      </Grid>
-      <Grid item xs={12}>
-        <Typography variant="h2">Services</Typography>
-      </Grid>
-      <Grid item xs={12}>
-        <Box marginTop={3} />
-      </Grid>
-      <Grid item container xs={12} spacing={5}>
-        {services.map(service => {
-          return (
-            <Grow in={true} key={service.name}>
-              <Grid item xs={12} sm={6} md={4}>
-                <Service {...service} />
-              </Grid>
-            </Grow>
-          )
-        })}
-      </Grid>
-      <Grid item xs={12}>
-        <Box marginTop={5} />
+      <Grid item xs={12} container spacing={5}>
+        <Grid item xs={12}>
+          <Typography variant="h2">Services</Typography>
+        </Grid>
+        <Grid item xs={7}>
+          <Box bgcolor="info.dark" height={300}></Box>
+        </Grid>
+        <Grid item xs={5}>
+          <Box bgcolor="info.dark" height={300}></Box>
+        </Grid>
+        <Grid item xs={4}>
+          <Box bgcolor="info.dark" height={300}></Box>
+        </Grid>
+        <Grid item xs={4}>
+          <Box bgcolor="info.dark" height={300}></Box>
+        </Grid>
+        <Grid item xs={4}>
+          <Box bgcolor="info.dark" height={300}></Box>
+        </Grid>
+        <Grid item xs={8}>
+          <Box bgcolor="info.dark" height={300}></Box>
+        </Grid>
+        <Grid item xs={4}>
+          <Box bgcolor="info.dark" height={300}></Box>
+        </Grid>
       </Grid>
     </>
   )
