f3d8fab72132e4854fb75d250f270a89215d3e43
BenLirio <benjlirio@gmail.com>
2020-02-24 11:18:29 -0500

    Base Layout

--- pages/services/index.js
+++ pages/services/index.js
@@ -12,22 +12,39 @@ const index = () => {
           <Typography variant="h2">Services</Typography>
         </Grid>
         <Grid item xs={7}>
-          <Box bgcolor="info.dark" height={300}></Box>
+          <Box bgcolor="info.dark" height={300}>
+            Before & after
+          </Box>
         </Grid>
         <Grid item xs={5}>
-          <Box bgcolor="info.dark" height={300}></Box>
+          <Box bgcolor="info.dark" height={300}>
+            Common Procedures
+          </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <Box bgcolor="info.dark" height={100}>
+            Call to Action
+          </Box>
         </Grid>
         <Grid item xs={4}>
-          <Box bgcolor="info.dark" height={300}></Box>
+          <Box bgcolor="info.dark" height={300}>
+            Feature Service 1
+          </Box>
         </Grid>
         <Grid item xs={4}>
-          <Box bgcolor="info.dark" height={300}></Box>
+          <Box bgcolor="info.dark" height={300}>
+            Feature Service 2
+          </Box>
         </Grid>
         <Grid item xs={4}>
-          <Box bgcolor="info.dark" height={300}></Box>
+          <Box bgcolor="info.dark" height={300}>
+            Feature Service 3
+          </Box>
         </Grid>
         <Grid item xs={8}>
-          <Box bgcolor="info.dark" height={300}></Box>
+          <Box bgcolor="info.dark" height={300}>
+            Service List
+          </Box>
         </Grid>
         <Grid item xs={4}>
           <Box bgcolor="info.dark" height={300}></Box>
