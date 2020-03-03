4acfbd69072ae0d5a3672941b79e07ab585e5334
BenLirio <benjlirio@gmail.com>
2020-03-03 16:49:46 -0500

    Contact spacing complete

--- components/Home/MapRow/MapRow.js
+++ components/Home/MapRow/MapRow.js
@@ -4,7 +4,8 @@ import {
   Card,
   CardMedia,
   Grid,
-  Typography
+  Typography,
+  CardContent
 } from '@material-ui/core'
 import React from 'react'
 import GoogleMap from './GoogleMap'
@@ -17,11 +18,13 @@ const MapRow = () => {
       </Grid>
       <Grid item xs={6}>
         <Typography variant="h4">Call: 616-281-8338</Typography>
-
-        <Typography variant="body1">
-          We know your time is valuable so at Healthy Smile, we offer convenient
-          office hours such as late evenings and even select Fridays
-        </Typography>
+        <CardContent>
+          <Typography variant="body1">
+            We know your time is valuable so at Healthy Smile, we offer
+            convenient office hours such as late evenings and even select
+            Fridays
+          </Typography>
+        </CardContent>
 
         <Button color="secondary">Contact</Button>
       </Grid>
