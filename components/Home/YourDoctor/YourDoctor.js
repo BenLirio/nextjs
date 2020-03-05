3b11ac4a471e59599b0f27de3e08ccea1d184b57
BenLirio <benjlirio@gmail.com>
2020-03-05 09:49:45 -0500

    About complete

--- components/Home/YourDoctor/YourDoctor.js
+++ components/Home/YourDoctor/YourDoctor.js
@@ -31,36 +31,39 @@ const YourDoctor = () => {
         <CardMedia image="/home/ona.jpg" style={{ height: '350px' }} />
       </Grid>
 
-      <Grid item xs={6}>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-            height: '100%'
-          }}
-        >
+      <Grid item xs={6} container>
+        <Grid item xs={12}>
           <Typography variant="h4">Dr. Suzzette Lirio Ona</Typography>
+        </Grid>
+        <Grid item xs={12}>
           <Typography variant="body1">
-            As a member of the American Dental Association for 18 years, with
-            extensive experience in hospital and cosmetic dentistry, Dr.
-            Suzzette Lirio Ona, D.M.D. understands the need for positive and
-            quality healthcare. She received a B.A. in Biochemistry from Mount
-            Holyoke College in 1989 and worked as a biochemist until 1994.
-            Desiring to provide service through dentistry, she attended Harvard
-            School of Dental Medicine and graduated Magna Cum Laude in 1998. She
-            spent her residency at the University of Washington Hospital,
-            treating mentally and physically challenged adults and children in
-            the operating room and in an emergency room setting.
+            Dr. Suzzette Lirio Ona, D.M.D received a B.A. in Biochemistry from
+            Mount Holyoke College in 1989 and went on to work as a
+            chemist/biochemist until 1994. After her work as a chemist she
+            attended Harvard School of Dental Medicine and graduated Magna Cum
+            Laude in 1998. She then went on to do a year of residency at the
+            University of Washington Hospital where she worked in depth treating
+            medically, mentally, and physically challenged adults and children
+            in the operating room and in an emergency room setting.
           </Typography>
-          <Box height={100}>
-            <div style={{ width: '100%', height: '100%', display: 'flex' }}>
+        </Grid>
+        <Grid item xs={12} container>
+          <Grid item xs={4}>
+            <Box height={100}>
               <Image src="home/harvard.png" size="contain" />
+            </Box>
+          </Grid>
+          <Grid item xs={4}>
+            <Box height={100}>
               <Image src="home/uofw.png" size="contain" />
+            </Box>
+          </Grid>
+          <Grid item xs={4}>
+            <Box height={100}>
               <Image src="home/mount.png" size="contain" />
-            </div>
-          </Box>
-        </div>
+            </Box>
+          </Grid>
+        </Grid>
       </Grid>
       <Grid item xs={3}></Grid>
     </>
